03cd2b095e1b5f1319cab6a19856a72af64da344
Yst0s <lacailleyanis@gmail.com>
2026-03-04 12:35:47 +0100

    update project

---
+++ ErAtlas/ErAtlas/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ErAtlas;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
